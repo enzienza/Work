29af92e380b871351db23bf7bd5a4d077c547076
EnzaEnzi <enza.webdev@gmail.com>
2022-09-19 00:09:29 +0200

    :sparkles: [back] new feature API : PersonalDetails

---
+++ inc/options-theme/API-index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Name file: API-index
+ * Description: group all API files of the theme here
+ *
+ * @package WordPress
+ * @subpackage MyWork
+ * @since 1.0.0
+ */
+
+/** ======================================================
+ * 01 - OP_Profile
+ *      group all profile files
+ */
+require_once ('OP_Profile/01-personalDetails.php');
